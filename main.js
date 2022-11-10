57327fd183779f36eb953b9ab5a14bc45b17c775
Aleksey Levada <aleksey.levada@gmail.com>
2022-11-11 00:12:34 +0300

    'start-js-logic'

---
+++ main.js
@@ -0,0 +1,13 @@
+const sliderImage = document.querySelector('.slider__image')
+const sliderTrack = document.querySelector('.slider__track')
+
+const prevBtn = document.querySelector('.slider__btn-prev')
+const nextBtn = document.querySelector('.slider__btn-next')
+
+prevBtn.addEventListener('click', () => {
+    console.log('prev click')
+})
+
+nextBtn.addEventListener('click', () => {
+    console.log('next click')
+})
