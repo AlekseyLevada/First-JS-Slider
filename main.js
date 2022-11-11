c492cab9eaee4cc658d02275e146d29539464a4b
Aleksey Levada <aleksey.levada@gmail.com>
2022-11-11 09:42:06 +0300

    'Complite-slider'

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+let position = 0
 const sliderImage = document.querySelector('.slider__image')
 const sliderTrack = document.querySelector('.slider__track')
 
@@ -5,9 +6,15 @@ const prevBtn = document.querySelector('.slider__btn-prev')
 const nextBtn = document.querySelector('.slider__btn-next')
 
 prevBtn.addEventListener('click', () => {
-    console.log('prev click')
+    position += 600
+    position > 0? position = -5400 : ''
+    sliderTrack.style.left = position + 'px'
+    
 })
 
 nextBtn.addEventListener('click', () => {
-    console.log('next click')
+    position -= 600
+    position < -5400? position = 0 : ''
+    sliderTrack.style.left = position + 'px'
+    
 })
